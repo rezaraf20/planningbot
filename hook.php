0bcc0ed43d40dd1ef5bb0ebf4b2760bc4f2d6c9d
Reza Rafiei <86382022+rezaraf20@users.noreply.github.com>
2025-07-21 20:35:10 +0330

    Update hook.php
    Added DB

--- hook.php
+++ hook.php
@@ -4,14 +4,21 @@ use Longman\TelegramBot\Telegram;
 
 $bot_api_key  = 'YOUR_BOT_API_TOKEN';
 $bot_username = 'YOUR_BOT_USERNAME';
+$db_host = 'localhost';
+$db_name = '*********';
+$db_user = '*********';
+$db_pass = '*********';
 
 try {
     $telegram = new Telegram($bot_api_key, $bot_username);
-
+    
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $telegram->enableExternalMySql($pdo);
     $telegram->addCommandsPaths([__DIR__ . '/Commands']);
-
+  
     $telegram->handle();
+
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    // لاگ خطا
     file_put_contents('bot-error.log', $e->getMessage(), FILE_APPEND);
 }
